4c57de47468b17151868fd4e14eeb7e1cba1b752
Satish Gunjal <gunjal.satish@gmail.com>
2020-03-24 20:15:14 +0530

    added sample config file

---
+++ config-example.js
@@ -0,0 +1,89 @@
+//Important: Set the env to either development or production to use the appropriate config
+/****************************************/
+const env = "development"; // 'development' or 'production'
+/*******************************************/
+
+let appRoot = require("app-root-path");
+const { format } = require("winston");
+//const { combine, timestamp, printf } = format;
+const timestampFormat = 'YYYY-MM-DD HH:mm:ss.SSS';
+
+//All Configurations
+const development = { 
+  projectId: 'gdfrestapi',
+  sessionIdTimeoutInMinutes: '2',
+  jsonKey:{
+    privateKey: 'private key from JSON file',
+    clientEmail: 'client email from JSON file'
+  },
+  logs: {
+    file: {
+      level: 'debug',
+      filename: `C:/Users/satish.gunjal/Google Drive/Projects/ChatBOT/EBL-Dia/1-Source-Code/Logs/EBL-DIA-Viber-Connector/%DATE%-trace.log`,
+      datePattern: "YYYY-MM-DD",
+      zippedArchive: false,
+      maxSize: '10m',
+      maxFiles: '30',
+      format: format.combine(
+          format.timestamp({format: timestampFormat}),
+          format.printf(
+              info => `${info.timestamp} [${info.logId}] ${info.level}: ${info.message}`
+          )
+      ),
+    },
+    console: {
+      level: 'info',
+      datePattern: "YYYY-MM-DD",
+      format: format.combine(
+        format.colorize(),
+        format.timestamp({format: timestampFormat}),
+        format.printf(
+        info => `${info.timestamp} [${info.logId}] ${info.level}: ${info.message}`
+        )
+      ),
+    },
+    morgan: {
+      format: ":method :url :status :res[content-length] - :response-time ms"
+    }
+  }
+};
+
+const production = {
+    logs: {
+      file: {
+        level: 'debug',
+        filename: `C:/Users/satish.gunjal/Google Drive/Projects/ChatBOT/EBL-Dia/1-Source-Code/Logs/EBL-DIA-Viber-Connector/%DATE%-trace.log`,
+        datePattern: "YYYY-MM-DD",
+        zippedArchive: false,
+        maxSize: '10m',
+        maxFiles: '30',
+        format: format.combine(
+            format.timestamp({format: timestampFormat}),
+            format.printf(
+                info => `${info.timestamp} [${info.logId}] ${info.level}: ${info.message}`
+            )
+        ),
+      },
+      console: {
+        level: 'info',
+        datePattern: "YYYY-MM-DD",
+        format: format.combine(
+          format.colorize(),
+          format.timestamp({format: timestampFormat}),
+          format.printf(
+          info => `${info.timestamp} [${info.logId}] ${info.level}: ${info.message}`
+          )
+        ),
+      },
+      morgan: {
+        format: ":method :url :status :res[content-length] - :response-time ms"
+      }
+    }
+  };
+
+const config = {
+  development,
+  production
+};
+
+module.exports = config[env];
